ca0cf49c2e96d6f26453cee72258b19d7cdfffea
OctoNik <nikolaus.535230113@stu.untar.ac.id>
2024-04-09 23:50:02 +0700

    third commit

--- node-express-mongodb-example/src/api/components/users/users-controller.js
+++ node-express-mongodb-example/src/api/components/users/users-controller.js
@@ -51,6 +51,14 @@ async function createUser(request, response, next) {
     const email = request.body.email;
     const password = request.body.password;
 
+    const emailExists = await usersService.checkEmailExists(email);
+    if (emailExists) {
+      throw errorResponder(
+        errorTypes.EMAIL_ALREADY_TAKEN,
+        'This email already taken, try use another'
+      );
+    }
+
     const success = await usersService.createUser(name, email, password);
     if (!success) {
       throw errorResponder(
